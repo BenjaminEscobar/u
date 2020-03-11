d65f354f549970502fb49f5ea03ccfa1c5f21359
BenjaminEscobar <lab.1805@gmail.com>
2020-03-11 17:13:31 -0600

    Proyecto Backend

---
+++ Backend/visionApp/src/main/java/com/lab/visionApp/VisionAppApplication.java
@@ -0,0 +1,13 @@
+package com.lab.visionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionAppApplication.class, args);
+	}
+
+}
